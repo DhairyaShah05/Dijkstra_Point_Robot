fa011832e4478fa976e70ae2fafad01419d4f16a
DhairyaShah05 <143757600+DhairyaShah05@users.noreply.github.com>
2024-03-10 01:39:17 -0500

    Update Dijkstra Algorithm and Plotting
    Optimized Dijkstra implementation and plotting method. Also solved issues of the previous commit.

--- dshah05_enpm661.py
+++ dshah05_enpm661.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import time
-import heapq 
+import heapq
 
 # Creating a Class for Node with it's parameters as X,Y Coordinates, Cost at each Node, it's Parent
 class Node:
@@ -214,82 +214,60 @@ def unique_id(node):
 # Dijkstra Algorithm
 
 def dijkstra_algorithm(start, goal, obstacle_space):
-    
-    ## Check if given Node is the Goal Node
-    if check_goal(start, goal):
+    if start.x == goal.x and start.y == goal.y:
         return None, 1
-    
-    # Storing as Nodes only when Start is not Goal
+
     goal_node = goal
     start_node = start
-    
-    # Data Handlers
     unexplored_nodes = {}
     all_nodes = []
-    explored_nodes = {} 
+    explored_nodes = {}
     open_list = []
-    
-    # All possible Movements for a Node
-    possible_moves = ['Up','UpRight','Right','DownRight','Down','DownLeft','Left','UpLeft']
-        
-    # Assigning Unique Key to Start Node
-    start_key = unique_id(start_node)
-    unexplored_nodes[(start_key)] = start_node
-    
-    # Push the Start Node into the Heap Queue
-    heapq.heappush(open_list, [start_node.cost, start_node])
-    
-    # Loops until all the Nodes have been Explored, i.e. until open_list becomes empty
-    while (len(open_list) != 0):
+    possible_moves = ['Up', 'UpRight', 'Right', 'DownRight', 'Down', 'DownLeft', 'Left', 'UpLeft']
 
-        current_node = (heapq.heappop(open_list))[1]
+    start_key = unique_id(start_node)
+    unexplored_nodes[start_key] = start_node
+    heapq.heappush(open_list, [start_node.cost, start_node])
+
+    while open_list:
+        current_node = heapq.heappop(open_list)[1]
         all_nodes.append([current_node.x, current_node.y])
         current_id = unique_id(current_node)
 
-        if check_goal(current_node, goal_node):
+        if current_node.x == goal_node.x and current_node.y == goal_node.y:
             goal_node.parent_node = current_node.parent_node
             goal_node.cost = current_node.cost
             print("Goal Node found")
-            return all_nodes,1
+            return all_nodes, 1
 
         if current_id in explored_nodes:
             continue
         else:
             explored_nodes[current_id] = current_node
-		
+        
         del unexplored_nodes[current_id]
         
-        # Looping through all possible nodes obtained through movements
-        for moves in possible_moves:
-            
-            # New x,y,cost after moving.
-            x,y,cost = move_node(moves, current_node.x, current_node.y, current_node.cost)
-            
-            # Updating Node with the New Values
+        for move in possible_moves:
+            x, y, cost = move_node(move, current_node.x, current_node.y, current_node.cost)
             new_node = Node(x, y, cost, current_node)
-            
-            # New ID
             new_node_id = unique_id(new_node)
             
-            # Checking Validity
-            if not check_valid(new_node.x, new_node.y, obstacle_space):
-                continue
-            elif new_node_id in explored_nodes:
+            if not check_valid(new_node.x, new_node.y, obstacle_space) or new_node_id in explored_nodes:
                 continue
             
-            # Updating Cost and Parent, if lesser cost is found.
             if new_node_id in unexplored_nodes:
                 if new_node.cost < unexplored_nodes[new_node_id].cost: 
                     unexplored_nodes[new_node_id].cost = new_node.cost
                     unexplored_nodes[new_node_id].parent_node = new_node.parent_node
             else:
                 unexplored_nodes[new_node_id] = new_node
-   			
-            # Pushing all the open nodes in Heap Queue to get the least costing node.
-            heapq.heappush(open_list, [ new_node.cost, new_node])
-   
-    return  all_nodes, 0
+            
+            heapq.heappush(open_list, [new_node.cost, new_node])
+    
+    return all_nodes, 0
 
+# Function to Back Track position from Goal Node to Start Node.
+    
 def back_track_path(goal_node):
     
     # Creating a Stack to hold the Backtracked Nodes
@@ -312,34 +290,50 @@ def back_track_path(goal_node):
     
     return x_path, y_path
 
+#This class is used to plot and see the explored nodes.
+
 class PathPlotter:
     def __init__(self, start_node, goal_node, obstacle_space):
         self.start_node = start_node
         self.goal_node = goal_node
         self.obstacle_space = obstacle_space
         
-        ## Plot the Start and Goal Positions
+        # Plot the Start and Goal Positions
         plt.plot(start_node.x, start_node.y, "Db")
         plt.plot(goal_node.x, goal_node.y, "Dg")
         
-        ## Plot Map
+        # Plot Map
         plt.imshow(obstacle_space, "seismic")
         self.ax = plt.gca()
         self.ax.invert_yaxis()
         
-    # Plot the explored nodes
+        # Store explored nodes for batch plotting
+        self.explored_nodes = []
+        self.explored_batch_size = 1000  # Adjust batch size as needed
+
     def plot_explored_nodes(self, explored_nodes):
-        # Plotting the Explored Nodes
-        for i in range(len(explored_nodes)):
-            plt.plot(explored_nodes[i][0], explored_nodes[i][1], "3y")
-                    
-    # Plot the shortest path    
+        self.explored_nodes.extend(explored_nodes)
+        
+        # Plot in batches for faster performance
+        if len(self.explored_nodes) >= self.explored_batch_size:
+            self._plot_batch()
+
+    def _plot_batch(self):
+        # Convert explored nodes to numpy array for vectorized plotting
+        explored_array = np.array(self.explored_nodes)
+        x_coords, y_coords = explored_array[:, 0], explored_array[:, 1]
+        plt.plot(x_coords, y_coords, "3y")
+        
+        # Clear explored nodes after plotting
+        self.explored_nodes = []
+
     def plot_path(self, x_path, y_path):
-        plt.plot(x_path, y_path,"--r")
+        plt.plot(x_path, y_path, "--r")
         plt.show()
         plt.close('all')
 
 # Main Function to Execute the Program
+
 if __name__ == '__main__':
     
     # Robot Space width and Height
