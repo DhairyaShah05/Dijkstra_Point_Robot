5b39e1fa48726f3928f8f97a2d7ad83da3462645
DhairyaShah05 <143757600+DhairyaShah05@users.noreply.github.com>
2024-03-09 22:44:34 -0500

    Map Update, Dijkstra and Plotter Implementation
    I faced issues with the location and buffer region of the hexagon and I tried to write a rough implementation of Dijkstra Algorithm. I created a new class for Plotting path and Obstacle space for visualization. The issues with this code is that the code works perfectly fine when given smaller inputs but does not compute end of corner inputs. The ram usage of my computer reaches 80% without any plot.

--- dshah05_enpm661.py
+++ dshah05_enpm661.py
@@ -87,7 +87,6 @@ def move_node(move,x,y,cost):
     else:
         return None
 
-
 # Function to define an obsatcle space and buffer space.
 
 def plot_map(width, height):
@@ -113,12 +112,12 @@ def plot_map(width, height):
             r24_buffer = (y - 5) - 400 
             
             # Hexagon Obstacle
-            h6_buffer = (y + 5) +  0.58*(x + 5) - 431.82
-            h5_buffer = (y + 5) + 0.58*(x - 5) - 231.72
-            h4_buffer = (x - 6.5) - 704.9
-            h3_buffer = (y - 5) + 0.58*(x - 5) - 731.78
-            h2_buffer = (y - 5) - 0.58*(x + 5) - 68.23
-            h1_buffer = (x + 6.5) - 445.1
+            h6_buffer = (y + 5) +  0.58*(x + 5) - 475.098
+            h5_buffer = (y + 5) - 0.58*(x - 5) + 275.002
+            h4_buffer = (x - 6.5) - 779.9
+            h3_buffer = (y - 5) + 0.58*(x - 5) - 775.002
+            h2_buffer = (y - 5) - 0.58*(x + 5) - 24.92
+            h1_buffer = (x + 6.5) - 520.1
             
             # Block Obstacle
             t1_buffer = (x + 5) - 900
@@ -130,7 +129,7 @@ def plot_map(width, height):
             t7_buffer = (y - 5) - 450
             
             # Setting the line constrain to obatain the obstacle space with buffer
-            if((t1_buffer>0 and t2_buffer<0 and t4_buffer>0 and t5_buffer<0) or(t2_buffer>0 and t3_buffer<0 and t4_buffer>0 and t7_buffer<0) or (t6_buffer>0 and t7_buffer<0 and t1_buffer>0 and t2_buffer<0) or (r11_buffer>0 and r12_buffer>0 and r13_buffer<0 and r14_buffer<0) or (r21_buffer>0 and r23_buffer<0 and r24_buffer<0 and r22_buffer>0) or (h6_buffer<0 and h5_buffer>0 and h4_buffer>0 and h3_buffer>0 and h2_buffer>0 and h1_buffer<0)):
+            if((t1_buffer>0 and t2_buffer<0 and t4_buffer>0 and t5_buffer<0) or(t2_buffer>0 and t3_buffer<0 and t4_buffer>0 and t7_buffer<0) or (t6_buffer>0 and t7_buffer<0 and t1_buffer>0 and t2_buffer<0) or (r11_buffer>0 and r12_buffer>0 and r13_buffer<0 and r14_buffer<0) or (r21_buffer>0 and r23_buffer<0 and r24_buffer<0 and r22_buffer>0) or (h6_buffer>0 and h5_buffer>0 and h4_buffer<0 and h3_buffer<0 and h2_buffer<0 and h1_buffer>0)):
                 obstacle_space[y, x] = 1
              
              
@@ -155,12 +154,12 @@ def plot_map(width, height):
             r23 = (y) - 400 
             
             # Hexagon Obstacle
-            h6 = (y) +  0.58*(x) - 431.82
-            h5 = (y) - 0.58*(x) + 231.72
-            h4 = (x) - 704.9
-            h3 = (y) + 0.58*(x) - 731.78
-            h2 = (y) - 0.58*(x) - 68.23
-            h1 = (x) - 445.1 
+            h6 = (y) +  0.58*(x) - 475.098
+            h5 = (y) - 0.58*(x) + 275.002
+            h4 = (x) - 779.9
+            h3 = (y) + 0.58*(x) - 775.002
+            h2 = (y) - 0.58*(x) - 24.92
+            h1 = (x) - 520.1 
             
             # Block Obstacle
             t1 = (x) - 900
@@ -177,7 +176,6 @@ def plot_map(width, height):
 
     return obstacle_space
 
-
 #Function to check if the Goal Node is Reached.
 
 def check_goal(current, goal): 
@@ -186,7 +184,6 @@ def check_goal(current, goal):
 		return True
 	else:
 		return False
-
 
 # Funtion to check if the move is valid.
 
@@ -207,3 +204,193 @@ def check_valid(x, y, obstacle_space):
 		except:
 			pass
 	return True
+
+# Generating a Unique ID
+
+def unique_id(node):
+    id = 3333*node.x + 113*node.y 
+    return id
+
+# Dijkstra Algorithm
+
+def dijkstra_algorithm(start, goal, obstacle_space):
+    
+    ## Check if given Node is the Goal Node
+    if check_goal(start, goal):
+        return None, 1
+    
+    # Storing as Nodes only when Start is not Goal
+    goal_node = goal
+    start_node = start
+    
+    # Data Handlers
+    unexplored_nodes = {}
+    all_nodes = []
+    explored_nodes = {} 
+    open_list = []
+    
+    # All possible Movements for a Node
+    possible_moves = ['Up','UpRight','Right','DownRight','Down','DownLeft','Left','UpLeft']
+        
+    # Assigning Unique Key to Start Node
+    start_key = unique_id(start_node)
+    unexplored_nodes[(start_key)] = start_node
+    
+    # Push the Start Node into the Heap Queue
+    heapq.heappush(open_list, [start_node.cost, start_node])
+    
+    # Loops until all the Nodes have been Explored, i.e. until open_list becomes empty
+    while (len(open_list) != 0):
+
+        current_node = (heapq.heappop(open_list))[1]
+        all_nodes.append([current_node.x, current_node.y])
+        current_id = unique_id(current_node)
+
+        if check_goal(current_node, goal_node):
+            goal_node.parent_node = current_node.parent_node
+            goal_node.cost = current_node.cost
+            print("Goal Node found")
+            return all_nodes,1
+
+        if current_id in explored_nodes:
+            continue
+        else:
+            explored_nodes[current_id] = current_node
+		
+        del unexplored_nodes[current_id]
+        
+        # Looping through all possible nodes obtained through movements
+        for moves in possible_moves:
+            
+            # New x,y,cost after moving.
+            x,y,cost = move_node(moves, current_node.x, current_node.y, current_node.cost)
+            
+            # Updating Node with the New Values
+            new_node = Node(x, y, cost, current_node)
+            
+            # New ID
+            new_node_id = unique_id(new_node)
+            
+            # Checking Validity
+            if not check_valid(new_node.x, new_node.y, obstacle_space):
+                continue
+            elif new_node_id in explored_nodes:
+                continue
+            
+            # Updating Cost and Parent, if lesser cost is found.
+            if new_node_id in unexplored_nodes:
+                if new_node.cost < unexplored_nodes[new_node_id].cost: 
+                    unexplored_nodes[new_node_id].cost = new_node.cost
+                    unexplored_nodes[new_node_id].parent_node = new_node.parent_node
+            else:
+                unexplored_nodes[new_node_id] = new_node
+   			
+            # Pushing all the open nodes in Heap Queue to get the least costing node.
+            heapq.heappush(open_list, [ new_node.cost, new_node])
+   
+    return  all_nodes, 0
+
+def back_track_path(goal_node):
+    
+    # Creating a Stack to hold the Backtracked Nodes
+    backtrack_stack = []
+    
+    # Storing the Goal Node
+    backtrack_stack.append(goal_node)
+    
+    # Backtracking to the Preceeding Nodes until Start Node is Reached
+    while backtrack_stack[-1].parent_node != -1:
+        parent = backtrack_stack[-1].parent_node
+        backtrack_stack.append(parent)
+        
+    # Since the Backtracking was done from Goal to Start Node, Reversing the Stack to get path from Start to Goal
+    backtrack_stack.reverse()
+    
+    # Extracting the x, y coordinates of the path from the Backtracked Nodes
+    x_path = [node.x for node in backtrack_stack]
+    y_path = [node.y for node in backtrack_stack]
+    
+    return x_path, y_path
+
+class PathPlotter:
+    def __init__(self, start_node, goal_node, obstacle_space):
+        self.start_node = start_node
+        self.goal_node = goal_node
+        self.obstacle_space = obstacle_space
+        
+        ## Plot the Start and Goal Positions
+        plt.plot(start_node.x, start_node.y, "Db")
+        plt.plot(goal_node.x, goal_node.y, "Dg")
+        
+        ## Plot Map
+        plt.imshow(obstacle_space, "seismic")
+        self.ax = plt.gca()
+        self.ax.invert_yaxis()
+        
+    # Plot the explored nodes
+    def plot_explored_nodes(self, explored_nodes):
+        # Plotting the Explored Nodes
+        for i in range(len(explored_nodes)):
+            plt.plot(explored_nodes[i][0], explored_nodes[i][1], "3y")
+                    
+    # Plot the shortest path    
+    def plot_path(self, x_path, y_path):
+        plt.plot(x_path, y_path,"--r")
+        plt.show()
+        plt.close('all')
+
+# Main Function to Execute the Program
+if __name__ == '__main__':
+    
+    # Robot Space width and Height
+    width = 1200
+    height = 500
+    
+    # Getting the Coordinates of Obstacles
+    obstacle_space = plot_map(width, height)
+    
+    # Getting the Start and Goal Coordinates from the User    
+    start_x = int(input("Enter Start Position's X Coordinate: "))
+    start_y = int(input("Enter Start Position's Y Coordinate: "))
+
+    goal_x = int(input("Enter Goal Position's X Coordinate: "))
+    goal_y = int(input("Enter Goal Position's Y Coordinate: "))
+    
+    # Start Timer to Check the Time of Execution
+    start_time = time.time()
+    
+    # Validating the Start Node if it is in Permitted Robot Space
+    if not check_valid(start_x, start_y, obstacle_space):
+        print("Enter Start Node within the permitted Robot Space")
+        exit(-1)
+    
+    # Validating the Goal Node if it is in Permitted Robot Space
+    if not check_valid(goal_x, goal_y, obstacle_space):
+        print("Enter Goal Node within the permitted Robot Space")
+        exit(-1)
+    
+    ## Creating Start Node which has attributes start (x,y) coordinates, cost = 0 and parent_node = -1
+    start_node = Node(start_x, start_y, 0.0, -1)
+    
+    ## Creating Goal Node which has attributes goal (x,y) coordinates, cost = 0 and parent_node = -1
+    goal_node = Node(goal_x, goal_y, 0.0, -1)
+    
+    # Implementing Dijkstra Algorithm
+    explored_nodes, goal_status = dijkstra_algorithm(start_node, goal_node, obstacle_space)
+    
+    # Back Track only if the Goal Node is reached.
+    if (goal_status == 1):
+        x_path, y_path = back_track_path(goal_node)
+        
+        # Plotting the Map, Obstacles and the Generated Path
+        plotter = PathPlotter(start_node, goal_node, obstacle_space)
+        plotter.plot_explored_nodes(explored_nodes)
+        plotter.plot_path(x_path, y_path)
+        cost = goal_node.cost
+        print(f"Cost of reaching the goal: {cost:.2f}")
+    else:
+        print("Goal could not be planned for the given points")
+
+    # Stopping the Timer after Program Execution
+    end_time = time.time()
+    print(f"Time Taken to Execute the Program is: {end_time - start_time}")
