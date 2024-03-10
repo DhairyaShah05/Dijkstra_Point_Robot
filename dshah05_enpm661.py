b758ea9db94ecdb70cbec0def06a9d5bce89442e
DhairyaShah05 <143757600+DhairyaShah05@users.noreply.github.com>
2024-03-09 19:32:48 -0500

    Add files via upload
    I have added a move_node function. This function accepts a movement direction as input and updates the position and cost accordingly. Additionally, a check_goal function is introduced to facilitate checking whether the current node has reached the goal node.

--- dshah05_enpm661.py
+++ dshah05_enpm661.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 
 class Node:
     
+    # Initialize Data
 	def __init__(self, x, y, cost, parent_node):
 
 		self.x = x
 		self.y = y
 		self.cost = cost
 		self.parent_node = parent_node
-  
+	
+    # To support Node Comparision
 	def __lt__(self,other):
 		return self.cost < other.cost
 
@@ -56,4 +58,30 @@ def move_downleft(x,y,cost):
 	y -= 1
 	cost += np.sqrt(2)
 	return x,y,cost
- 
+
+def move_node(move,x,y,cost):
+    if move == 'Up': 
+        return move_up(x,y,cost)
+    elif move == 'UpRight':   
+        return move_upright(x,y,cost)
+    elif move == 'Right':   
+        return move_right(x,y,cost)
+    elif move == 'DownRight':   
+        return move_downright(x,y,cost)
+    elif move == 'Down':   
+        return move_down(x,y,cost)
+    elif move == 'DownLeft':    
+        return move_downleft(x,y,cost)
+    elif move == 'Left':    
+        return move_left(x,y,cost)
+    elif move == 'UpLeft':    # Move upleft
+        return move_upleft(x,y,cost)
+    else:
+        return None
+
+def check_goal(current, goal): 
+    
+	if (current.x == goal.x) and (current.y == goal.y):
+		return True
+	else:
+		return False
